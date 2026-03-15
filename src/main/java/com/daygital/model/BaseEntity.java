3c81c8ad131fc08f7971369e22096f27ee1b831b
dayanasimoes2011-svg <dayanasimoes2011@gmail.com>
2026-03-15 10:59:00 -0300

    Create BaseEntity class

---
+++ src/main/java/com/daygital/model/BaseEntity.java
@@ -0,0 +1,31 @@
+public class BaseEntity {
+    // Attributes
+    private Long id;
+    private String createdAt;
+    private String updatedAt;
+
+    // Methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
